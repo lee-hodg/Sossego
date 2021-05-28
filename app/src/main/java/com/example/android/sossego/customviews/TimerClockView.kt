eb6a9511eb8d6416a2d1567bb373dbd49312581f
lee <lee@logicon.io>
2021-05-28 20:08:00 -0300

    The base animation for mediation

--- app/src/main/java/com/example/android/sossego/customviews/TimerClockView.kt
+++ app/src/main/java/com/example/android/sossego/customviews/TimerClockView.kt
@@ -2,17 +2,12 @@ package com.example.android.sossego.customviews
 
 import android.content.Context
 import android.graphics.*
-import android.os.SystemClock
+import android.os.Build
 import android.text.format.DateUtils
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.withStyledAttributes
-import com.example.android.sossego.R
+import androidx.annotation.RequiresApi
 import timber.log.Timber
-import java.lang.Long.getLong
-import kotlin.math.cos
-import kotlin.math.sin
-
 
 
 class TimerClockView @JvmOverloads constructor(
@@ -26,9 +21,9 @@ class TimerClockView @JvmOverloads constructor(
     }
 
     private var radius = 0.0f                   // Radius of the circle.
-    // position variable which will be used to draw label and indicator circle position
-    private val startTickPosition: PointF = PointF(0.0f, 0.0f)
-    private val endTickPosition: PointF = PointF(0.0f, 0.0f)
+
+    // Override the setters so that we invalidate the view and call onDraw if the live data
+    // changed
     private var _elapsedSeconds: Long = 0L
     var elapsedSeconds: Long
         get() = _elapsedSeconds
@@ -44,17 +39,17 @@ class TimerClockView @JvmOverloads constructor(
             invalidate()
         }
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
-        color = Color.BLACK
+        color = Color.WHITE
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create( "", Typeface.BOLD)
     }
 
-    private val tickPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val donutPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
-        color = Color.RED
+        color = Color.BLUE
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create( "", Typeface.BOLD)
@@ -62,26 +57,17 @@ class TimerClockView @JvmOverloads constructor(
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
-        color = Color.GRAY
+        color = Color.LTGRAY
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create( "", Typeface.BOLD)
     }
 
     init {
+        // otherwise top controllers don't work
         isClickable = false
 
     }
-
-//    override fun performClick(): Boolean {
-//        if (super.performClick()) return true
-//
-//        fanSpeed = fanSpeed.next()
-//        contentDescription = resources.getString(fanSpeed.label)
-//
-//        invalidate()
-//        return true
-//    }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         // circle radius will be 80% of smallest height or width dimension (half)
@@ -90,30 +76,33 @@ class TimerClockView @JvmOverloads constructor(
         radius = (kotlin.math.min(width, height) / 2.0 * 0.8).toFloat()
     }
 
-//    private fun PointF.computeXYForTick(pos: Int, radius: Float) {
-//        // Angles are in radians.
-//        val angle = (pos.toFloat()/totalMinutes.toFloat()) * (2*Math.PI)
-//        x = (radius * cos(angle)).toFloat() + width / 2
-//        y = (radius * sin(angle)).toFloat() + height / 2
-//        Timber.tag(TAG).d("computed angle $angle, x: $x, y: $y for pos $pos and radius $radius")
-//    }
-
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        // Draw the dial.
-        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+        val arcThickness = radius * 0.10f
+        val outerRadius = radius + arcThickness
 
+        // Draw the donut.
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), outerRadius, donutPaint)
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, circlePaint)
 
         Timber.tag(TAG).d("Got elapsedSeconds $elapsedSeconds and selectedInt $selectedInterval")
 
         if(elapsedSeconds != 0L && selectedInterval != 0L) {
             val elapsedSecondsTemp = elapsedSeconds/1000
-            // Draw inner shrinking circle
-            val innerRadius = (elapsedSeconds.toFloat()/selectedInterval.toFloat()) * radius
-            Timber.tag(TAG).d("draw w/ innerRadius $innerRadius")
+            // fraction of the interval elapsed determines the sweepAngle (degrees)
+            val fractionComplete = 1.0f - (elapsedSeconds.toFloat()/selectedInterval.toFloat())
+            val sweepAngle = 360.0f * fractionComplete
+            Timber.tag(TAG).d("draw w/ fractionComplete $fractionComplete and sweepAngle $sweepAngle")
+            canvas.drawArc((width / 2).toFloat()- outerRadius,
+                (height / 2).toFloat() - outerRadius,
+                (width / 2).toFloat() + outerRadius,
+                (height / 2).toFloat() +outerRadius,
+                -90.0f, -sweepAngle, true, textPaint)
 
-            canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), innerRadius, tickPaint)
+            canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, circlePaint)
 
+            // Draw timer text at center
             val timeText =
                 if (elapsedSecondsTemp < 60) elapsedSecondsTemp.toString() else DateUtils.formatElapsedTime(
                     elapsedSecondsTemp
@@ -122,16 +111,5 @@ class TimerClockView @JvmOverloads constructor(
             canvas.drawText(timeText, (width / 2).toFloat(), (height / 2).toFloat(), textPaint)
         }
 
-        // Draw the ticks.
-//        val tickStartRadius = (0.80 * radius).toFloat()
-//        val tickEndRadius = radius
-//
-//        for (i in 1..totalMinutes) {
-//            startTickPosition.computeXYForTick(i, tickStartRadius)
-//            endTickPosition.computeXYForTick(i, tickEndRadius)
-//            Timber.tag(TAG).d("$i Draw tick with start (x: ${startTickPosition.x}, y: ${startTickPosition.y}) and end (x: ${endTickPosition.x} , y: ${endTickPosition.y})")
-//            canvas.drawLine(startTickPosition.x, startTickPosition.y, endTickPosition.x,
-//                endTickPosition.y, tickPaint)
-//        }
     }
 }
