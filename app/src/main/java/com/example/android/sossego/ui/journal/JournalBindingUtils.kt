e75b9a9e7c7a32bca2b3db383769a0d126c9767c
lee <lee@logicon.io>
2021-06-12 09:34:25 -0300

    - Add glide - bindImage databinding that uses glide - use random image from picsum in journal detail - loading and broken drawables

--- app/src/main/java/com/example/android/sossego/ui/journal/JournalBindingUtils.kt
+++ app/src/main/java/com/example/android/sossego/ui/journal/JournalBindingUtils.kt
@@ -1,8 +1,13 @@
 package com.example.android.sossego.ui.journal
 
 
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.android.sossego.R
 import com.example.android.sossego.convertLongToDateString
 import com.example.android.sossego.database.journal.FirebaseJournalEntry
 
@@ -26,5 +31,22 @@ fun TextView.doJournalEntrySmartTruncate(journalEntry: FirebaseJournalEntry?) {
 fun TextView.setJournalEntryCreatedDate(item: FirebaseJournalEntry?) {
     item?.let {
         text = convertLongToDateString(item.createdDate)
+    }
+}
+
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+@BindingAdapter("setImageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .into(imgView)
     }
 }
